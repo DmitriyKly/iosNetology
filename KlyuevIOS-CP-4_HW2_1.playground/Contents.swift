ad2b3f8392a24217d758d7a395e56cb9822ea5fa
Dmitriy <Dimankl@yandex.ru>
2023-01-31 14:59:56 +0300

    add Google Analytics

--- KlyuevIOS-CP-4_HW2_1.playground/Contents.swift
+++ KlyuevIOS-CP-4_HW2_1.playground/Contents.swift
@@ -79,3 +79,5 @@ func resultTime01(Begin first: String, last: String) -> Int {
     return [ostNum01[first], ostNum01[last]].compactMap{$0}.sorted().reduce(0, {$1 - $0})
 }
 resultTime(Begin: "ost3", last: "ost1")
+
+// Здесь типо скрипт Google Analytics
